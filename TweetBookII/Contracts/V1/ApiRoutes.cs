cd0f2427e8fc25610939da4fe591a1a79ad6d269
andrey kurochkin <andrey.kurochkin@hotmail.com>
2022-04-13 19:21:45 +0300

    added create post controller without request and response

--- TweetBookII/Contracts/V1/ApiRoutes.cs
+++ TweetBookII/Contracts/V1/ApiRoutes.cs
@@ -4,9 +4,13 @@ public static class ApiRoutes
 {
     public const string Version = "v1";
     public const string Root = "api";
-    public const string Base = Root + Version;
+    public const string Base = Root + "/" + Version;
     public static class Posts
     {
         public const string GetAll = Base + "/posts";
+
+        public const string Create = Base + "/posts";
+
+        public const string Get = Base + "/posts/{id}";
     }
 }
--- TweetBookII/Controllers/V1/PostsController.cs
+++ TweetBookII/Controllers/V1/PostsController.cs
@@ -12,9 +12,9 @@ public class PostsController : Controller
     {
         Enumerable.Range(0, 5)
             .ToList()
-            .ForEach(_ => _posts.Add(new Post 
-            { 
-                Id = Guid.NewGuid().ToString() 
+            .ForEach(_ => _posts.Add(new Post
+            {
+                Id = Guid.NewGuid().ToString()
             }));
     }
 
@@ -22,5 +22,19 @@ public class PostsController : Controller
     public IActionResult GetAll()
     {
         return Ok(_posts);
+    }
+
+    [HttpPost(ApiRoutes.Posts.Create)]
+    public IActionResult Create([FromBody] Post post)
+    {
+        if (string.IsNullOrEmpty(post.Id))
+        {
+            post.Id = Guid.NewGuid().ToString();
+        }
+
+        var baseUri = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.ToUriComponent()}";
+        var responseUri = $"{baseUri}/{ApiRoutes.Posts.Get.Replace("{id}", post.Id)}";
+
+        return Created(responseUri, post);
     }
 }
